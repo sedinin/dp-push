49f932e4ce265dcb98495e4550ad2a84366d35ad
Yura Zhloba <yzh44yzh@gmail.com>
2012-07-19 15:58:32 +0300

    initial import

---
+++ Makefile
@@ -0,0 +1,21 @@
+ERL_RUN_ARGS:=-pa ebin -config elog -boot start_sasl -s rolltogo_app main
+
+compile: get-deps
+	rebar compile
+
+get-deps:
+	rebar get-deps
+
+clean:
+	rebar clean
+	rm -f erl_crash.dump
+
+test:	compile
+	rebar eunit skip_deps=true
+
+run:
+	ERL_LIBS=deps:apps erl $(ERL_RUN_ARGS)
+
+background:
+	ERL_LIBS=deps:apps erl -detached $(ERL_RUN_ARGS)
+
---
+++ apps/dp_push/include/logger.hrl
@@ -0,0 +1,15 @@
+%% -define(LOGGER, true).
+
+-ifdef(LOGGER).
+-define(INFO(Format, Data), error_logger:info_msg(Format, Data)).
+-define(INFO_(Format), error_logger:info_msg(Format)).
+-else.
+-define(INFO(Format, Data), do_nothing).
+-define(INFO_(Format), do_nothing).
+-endif.
+
+-define(WARN(Format, Data), error_logger:info_msg(Format, Data)).
+-define(WARN_(Format), error_logger:info_msg(Format)).
+
+-define(ERROR(Format, Data), error_logger:error_msg(Format, Data)).
+-define(ERROR_(Format), error_logger:error_msg(Format)).
